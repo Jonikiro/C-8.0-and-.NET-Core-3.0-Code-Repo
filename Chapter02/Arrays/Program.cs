7e4b9f275982f5e8d37390c24d074fd7058f67c2
Jonikiro <mcabrera122@gmail.com>
2020-01-29 16:18:57 -0600

    Initial Commit

---
+++ Chapter02/Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names;
+            names = new string[4];
+            int[] numbers = new int[4];
+
+            for (int i = 0; i < 4; i++) {
+                names[i] = Console.ReadLine();
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            foreach(string s in names) {
+                Console.WriteLine(s);
+            }
+            foreach(int x in numbers) {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
---
+++ Chapter02/Variables/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object height = 1.88;
+            object name = "Amir";
+            Console.WriteLine($"{name} is {height} metres tall.");
+
+            // int length1 = name.Length;
+            int length2 = ((string)name).Length;
+            Console.WriteLine($"{name} has {length2} characters.");
+
+            dynamic anotherName = "Ahmed";
+            int length = anotherName.Length;
+            Console.WriteLine($"{length}");
+
+            var population = 66_000_000;
+            var weight = 1.88;
+            var price = 4.99M;
+            var fruit = "Apples";
+            var letter = 'Z';
+            var happy = true;
+
+            var xml1 = new XmlDocument();
+            XmlDocument xml2 = new XmlDocument();
+            try {
+                var file1 = File.CreateText(@"C:\something.txt");
+                StreamWriter file2 = File.CreateText(@"C:\something.txt");
+            } catch(Exception e) {Console.WriteLine($"Oh well");}
+
+
+            Console.WriteLine($"default(int) = {default(int)}");
+            Console.WriteLine($"default(bool) = {default(bool)}");
+            Console.WriteLine($"default(DateTime) = {default(DateTime)}");
+            Console.WriteLine($"default(string) = {default(string)}");
+        }
+    }
+}
