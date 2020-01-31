641cda513a3274906ca89515267623fea6df11cd
Jonikiro <mcabrera122@gmail.com>
2020-01-31 13:34:17 -0600

    Added another chapter 3 exercise

--- Chapter03/SelectionStatements/Program.cs
+++ Chapter03/SelectionStatements/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using static System.Console;
+using System.IO;
 
 namespace SelectionStatements {
     class Program {
@@ -17,6 +18,65 @@ namespace SelectionStatements {
             } else {
                 WriteLine("o is not an int, so it cannot multiply!");
             }
+
+            A_label:
+            var number = (new Random()).Next(1,7);
+            WriteLine($"My random number is {number}");
+            switch(number) {
+                case 1:
+                    WriteLine("One");
+                    break;
+                case 2:
+                    WriteLine("Two");
+                    goto case 1;
+                case 3:
+                case 4:
+                    WriteLine("Three or four");
+                    goto case 1;
+                case 5:
+                    System.Threading.Thread.Sleep(500);
+                    goto A_label;
+                default:
+                    WriteLine("Default");
+                    break;
+            }
+
+            string path = @"C:\Users\mcabr\Downloads\Programming\C#\Book Code\Chapter03";
+            Stream s = File.Open(Path.Combine(path, "file.txt"),
+                                 FileMode.OpenOrCreate);
+            string message = string.Empty;
+            switch(s) {
+                case FileStream writeableFile when writeableFile.CanWrite:
+                    message = "The stream is a file that I can write to.";
+                    break;
+                case FileStream readonlyFile:
+                    message = "The stream is a read-only file.";
+                    break;
+                case MemoryStream ms:
+                    message = "The stream is a memory address.";
+                    break;
+                default:
+                    message = "The stream is some other type.";
+                    break;
+                case null:
+                    message = "The stream is null.";
+                    break;
+            }
+            WriteLine(message);
+
+            message = s switch {
+                FileStream writeableFile when s.CanWrite
+                    => "The stream is a file that I can write to.",
+                FileStream readonlyFile
+                    => "The stream is a read-only file.",
+                MemoryStream ms
+                    => "The stream is a memory address.",
+                null
+                    => "The stream is null.",
+                _
+                    => "The stream is some other type."
+            };
+            WriteLine(message);
         }
     }
 }
